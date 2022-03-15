3f62cc0634ae15d5e3844e454698670b3a23a356
DiegoPonte <diego.ponte@xpand-it.com>
2022-03-15 17:46:46 +0000

    angular.json names update

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/teteu-frontend'),
+      dir: require('path').join(__dirname, './coverage/commercial-panel'),
       subdir: '.',
       reporters: [
         { type: 'html' },
