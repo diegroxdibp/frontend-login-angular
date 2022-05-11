23e84dec8ecc9ba62421b96e4322e2d60ce525a2
DiegoPonte <diego.ponte@xpand-it.com>
2022-05-11 19:58:34 +0100

    https to endpoint

--- src/app/.env.ts
+++ src/app/.env.ts
@@ -1,2 +1,2 @@
-export const API_ENDPOINT = 'http://vps36197.publiccloud.com.br:3000/';
+export const API_ENDPOINT = 'https://vps36197.publiccloud.com.br:3000/';
 // export const API_ENDPOINT = 'http://localhost:3000/';
